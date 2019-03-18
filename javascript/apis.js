40ed86ecfa7185042e443f50390ae68e27f81430
Ninja <kwvinberg@gmail.com>
2019-03-18 12:48:46 -0700

    button values

--- javascript/apis.js
+++ javascript/apis.js
@@ -7,23 +7,28 @@
 
 $(document).ready(function () {
   var config = {
-apiKey: "AIzaSyBFvIwDGuvgIoxHnmSgUzbAmzW8gvpnxKo",
-authDomain: "melos-71bca.firebaseapp.com",
-databaseURL: "https://melos-71bca.firebaseio.com",
-projectId: "melos-71bca",
-storageBucket: "melos-71bca.appspot.com",
-messagingSenderId: "197405510515"
-
+    apiKey: "AIzaSyBFvIwDGuvgIoxHnmSgUzbAmzW8gvpnxKo",
+    authDomain: "melos-71bca.firebaseapp.com",
+    databaseURL: "https://melos-71bca.firebaseio.com",
+    projectId: "melos-71bca",
+    storageBucket: "melos-71bca.appspot.com",
+    messagingSenderId: "197405510515"
 };
+// get user inputs
+var musicPref = $("#musicP").val()
+var weatherPref =  $("#weatherP").val()
+var mood = $("#moodP").val()
+
 firebase.initializeApp(config);
 var database = firebase.database();
+var weatherDescription;
+
 
 //===============AJAX===========================//
 function getWeather(){
-var zipCode= $('#postal-code').val();
+  var zipCode= $('#postal-code').val();
 console.log(zipCode)
 $.ajax({
-
 	url: "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&appid=8caea81085fc66df0fb0c7d61c6772b8",
 	method:'GET',
 }).then(function(response){
@@ -32,33 +37,37 @@ $.ajax({
 		var cloud = response.list[i].clouds.all;
 		console.log(cloud);
 
-		var weatherDescription = response.list[i].weather[0].main;
+		weatherDescription = response.list[i].weather[0].main;
 		console.log(weatherDescription)
 
 		var temperature = response.list[i].main.temp;
 		console.log(temperature)
 
 		var date = response.list[i].dt_txt;
-		console.log(date);
+    console.log(date);
+    // database.ref().set({ "zipcode" : zipCode });
+    database.ref().set({ "weather" : weatherDescription })
 	}
 })
 }
+$("#musicP").on("click", function (){
+    database.ref().set({"Music Pref:": musicPref})
+})
 
 
-//store weather data locally using firebase
-$(".btn-primary").on("click", function (event) {
-event.preventDefault();
+//corralate user info to generate playlist
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+    $("#postal-code").html("")
+    getWeather();
+    // if ({
+    //   weatherDescription = clear,
+      
 
-
-
-var zipCode = $("#postal-code").val();
-$("#postal-code").html("")
-database.ref().set({ "zipcode": zipCode });
-console.log(zipCode)
-getWeather();
+    // });
 });
 //hit spotify api
-//GET user authentication
+
 //hit face recog
 
   var video = document.querySelector("#videoElement");

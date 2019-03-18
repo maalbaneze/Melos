8a5258625ddec2c70f5cb55c059cbb8e5227b2da
Dennis <dennissarmiento808@gmail.com>
2019-03-18 10:28:11 -0700

    up

--- javascript/apis.js
+++ javascript/apis.js
@@ -4,7 +4,6 @@
 //         URL: api.openweathermap.org
 // Giphy Key: i5u86hxDFgUmIDDbuFDZ6vsL4wBLI8B6
 //      URL: api.giphy.com
-<<<<<<< Updated upstream
 
 $(document).ready(function () {
   var config = {
